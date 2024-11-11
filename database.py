4be347da7cb77feea6d4c9418462ba8a8e1ddc8d
Brendaandradecz <brendabarbosaand@gmail.com>
2024-11-11 13:06:53 -0300

    Projeto final

--- database.py
+++ database.py
@@ -1,16 +1,18 @@
 import psycopg2
-from psycopg2 import OperationalError
 
 def get_db_connection():
-    try:
-        conn = psycopg2.connect(
-            dbname='flask_project',
-            user='postgres',
-            password='docemingau',
-            host='localhost',
-            port='5433'
-        )
-        return conn
-    except OperationalError as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
-        return None
+    conn = psycopg2.connect(
+        dbname='flask_project1',
+        user='postgres',
+        password='docemingau',
+        host='localhost',
+        port='5433',
+        options="-c client_encoding=UTF8"
+    )
+    return conn
+try:
+    conn = get_db_connection()
+    print("Conexão ao banco de dados estabelecida com sucesso!")
+    conn.close()
+except Exception as e:
+    print(f"Erro ao conectar ao banco de dados: {e}")
